190c8626a28db66f481f7c4a2f5fc9b7c8387ec0
advocatediablo <advocatediablo@mail.com>
2021-01-21 20:58:23 -0500

    upated to have EJS

--- modules/alb/alb.tf
+++ modules/alb/alb.tf
@@ -1,6 +1,6 @@
 # security group for application load balancer
-resource "aws_security_group" "example_alb_sg" {
-  name        = "nginx-example-alb-sg"
+resource "aws_security_group" "EJS_alb_sg" {
+  name        = "nginx-EJS-alb-sg"
   description = "allow incoming HTTP traffic only"
   vpc_id      = var.vpc_id
 
@@ -18,26 +18,26 @@ resource "aws_security_group" "example_alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    Name = "alb-security-group-example"
+    Name = "alb-security-group-EJS"
   }
 }
 
 # using ALB - instances in private subnets
-resource "aws_lb" "example_lb" {
-  name               = "example-lb-tf"
+resource "aws_lb" "EJS_lb" {
+  name               = "EJS-lb-tf"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.example_alb_sg.id]
+  security_groups    = [aws_security_group.EJS_alb_sg.id]
   subnets            = var.public_subnets
 
   tags = {
-    Name = "example-lb-tf"
+    Name = "EJS-lb-tf"
   }
 }
 
 # alb target group
-resource "aws_alb_target_group" "docker-example-tg" {
-  name     = "docker-example-alb-target-group"
+resource "aws_alb_target_group" "docker-EJS-tg" {
+  name     = "docker-EJS-alb-target-group"
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
@@ -49,26 +49,26 @@ resource "aws_alb_target_group" "docker-example-tg" {
 
 # listener
 resource "aws_alb_listener" "http_listener" {
-  load_balancer_arn = aws_lb.example_lb.arn
+  load_balancer_arn = aws_lb.EJS_lb.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
-    target_group_arn = aws_alb_target_group.docker-example-tg.arn
+    target_group_arn = aws_alb_target_group.docker-EJS-tg.arn
     type             = "forward"
   }
 }
 
 # target group attach
 # using nested interpolation functions and the count parameter to the "aws_alb_target_group_attachment"
-resource "aws_lb_target_group_attachment" "docker-example" {
+resource "aws_lb_target_group_attachment" "docker-EJS" {
   count            = length(var.azs)
-  target_group_arn = aws_alb_target_group.docker-example-tg.arn
+  target_group_arn = aws_alb_target_group.docker-EJS-tg.arn
   target_id        = element(split(",", join(",", var.instance_ids)), count.index)
   port             = 80
 }
 
 # ALB DNS is generated dynamically, return URL so that it can be used
 output "url" {
-  value = "http://${aws_lb.example_lb.dns_name}/"
+  value = "http://${aws_lb.EJS_lb.dns_name}/"
 }
